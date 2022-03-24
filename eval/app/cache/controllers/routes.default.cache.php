bfcbbd27e1b13e76da7828a0aac48a4651338005
Yohann <22004112@etu.unicaen.fr>
2022-03-24 15:16:35 +0100

    Route section/idSection marche

---
+++ eval/app/cache/controllers/routes.default.cache.php
@@ -0,0 +1,2 @@
+<?php
+return array("/_default/"=>["controller"=>"controllers\\StoreController","action"=>"index","parameters"=>[],"name"=>"home","cache"=>false,"duration"=>0],"/section/(.+?)/"=>["controller"=>"controllers\\StoreController","action"=>"section","parameters"=>[0],"name"=>"section","cache"=>false,"duration"=>0]);
--- eval/app/controllers/StoreController.php
+++ eval/app/controllers/StoreController.php
@@ -2,6 +2,7 @@
 namespace controllers;
  use models\Product;
  use models\Section;
+ use Ubiquity\attributes\items\router\Route;
  use Ubiquity\orm\DAO;
 
  /**
@@ -12,6 +13,13 @@ class StoreController extends \controllers\ControllerBase{
     #[Route('_default', name: 'home')]
 	public function index(){
         $Section=DAO::getAll(Section::class);
-        $this->loadView('main/StoreController.html',compact("Section");
+        $this->loadView('main/StoreController.html',compact("Section"));
 	}
+
+    #[Route(path: "section/{id}",name: "section")]
+    public function section($id){
+        $Section=DAO::getById(Section::class,$id+1);
+        $Product=$Section->getProducts();
+        $this->loadView('main/Produits.html',compact("Section","Product"));
+    }
 }
