4a395e803e3e488e5dddd551e18ca3bc6e26cef6
Yohann <22004112@etu.unicaen.fr>
2022-03-24 14:47:12 +0100

    Route par défaut ainsi que affichage des sections

--- eval/app/controllers/StoreController.php
+++ eval/app/controllers/StoreController.php
@@ -1,11 +1,17 @@
 <?php
 namespace controllers;
+ use models\Product;
+ use models\Section;
+ use Ubiquity\orm\DAO;
+
  /**
   * Controller StoreController
   */
 class StoreController extends \controllers\ControllerBase{
 
+    #[Route('_default', name: 'home')]
 	public function index(){
-		
+        $Section=DAO::getAll(Section::class);
+        $this->loadView('main/StoreController.html',compact("Section");
 	}
 }
